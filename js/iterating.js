bfbcb68c82fe67f4b66c45ea95bd7cd135ca6613
Ezra Marama <ezra.marama@gmail.com>
2023-02-17 10:03:39 -0600

    adding iterating_arrays_js.html & iterating.js

--- js/iterating.js
+++ js/iterating.js
@@ -26,7 +26,10 @@
      */
 
     console.log("3rd todo:");
-    console.log(names[0],names[1],names[2],names[3]);
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
