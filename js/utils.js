0c667a4722e11bec71c4eecc9136c36437bd7e1d
Ezra Marama <ezra.marama@gmail.com>
2023-03-14 12:08:09 -0500

    updating dom-query-js.html for DOM Event exercises, adding js dropdown code to utils.js, removing images from gitignore

--- js/utils.js
+++ js/utils.js
@@ -74,3 +74,27 @@ const arrayOfRandomNumbers = (lengthOfArray) =>{
     return array;
 
 }
+
+//DOM JS
+// -------------------DROP DOWN MENU JS-------------------
+let dropdowns = document.querySelectorAll('[data-dropdown="parent"]');
+dropdowns.forEach(function(dropdown){
+    let toggle = dropdown.querySelector('[data-dropdown="toggle"]');
+    toggle.addEventListener('click', function(){
+        dropdowns.forEach(function(element){
+            // if already has class of open and is not this element, remove it
+            if (element.classList.contains('open') && element !== dropdown) {
+                element.classList.remove('open');
+            }
+        })
+        dropdown.classList.toggle('open');
+    });
+});
+pageWrapper.addEventListener('click', function(event){
+    //if the event target is not a dropdown, close all dropdowns
+    if (!event.target.closest('[data-dropdown="parent"]')) {
+        dropdowns.forEach(function(dropdown){
+            dropdown.classList.remove('open');
+        });
+    }
+});
