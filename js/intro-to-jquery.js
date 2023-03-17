170ee869c0e2a461334c5ec4991fd5125dc848d4
Ezra Marama <ezra.marama@gmail.com>
2023-03-17 15:19:39 -0500

    updating jquery_exercises.html for mouse events exercise

--- js/intro-to-jquery.js
+++ js/intro-to-jquery.js
@@ -18,18 +18,48 @@ $(function() {
     //     let alertMsg = $('#alert-me').text();
     //     alert(alertMsg);
     // });
-
+    //
     // $('.codeup').css({
     //     'border' : '1px solid red'
     // });
 
-    $('.shrink-me').css({'width' : '100px'});
+    // $('.shrink-me').css({'width' : '100px'});
+    //
+    // $('li').css({'font-size': '20px'});
+    //
+    // $('h1, p, li').css({'background-color' : 'pink'});
+    // $(function () {
+    //     alert($('h1').html());
+    // })
 
-    $('li').css({'font-size': '20px'});
+    $('h1').click(
+        function (){
+            $(this).css({
+                'background-color' : 'blue'
+            });
+        }
+    );
+    $('p').dblclick(
+        function (){
+            $(this).css({
+                'font-size' : '18px'
+            });
+        }
+    );
+    $('li').hover(
+        function (){
+            $(this).css({
+                'color' : 'red'
+            })
+        },
+        function (){
+            $(this).css({
+                'color' : 'black'
+            })
+        }
+    );
 
-    $('h1, p, li').css({'background-color' : 'pink'});
-    $(function () {
-        alert($('h1').html());
-    })
+
+
 
 });
