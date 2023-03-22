c40c31ffb0decd2329934e75b6dbf384dbeb4772
Ezra Marama <ezra.marama@gmail.com>
2023-03-22 17:03:09 -0500

    adding mapbox exercise files

---
+++ js/mapbox-exercise.js
@@ -0,0 +1,19 @@
+document.querySelector('#zoomSubmit').addEventListener('click', event => {
+    event.preventDefault();
+    map.setZoom(document.querySelector('#zoom').value);
+});
+
+document.querySelector('#setMarkerButton').addEventListener('click', event => {
+
+    event.preventDefault();
+    const address = document.querySelector('#setMarker').value;
+    console.log(address);
+
+    geocode(address, MAPBOX_API_TOKEN).then(coords => {
+        console.log(coords);
+        const newMarker = new mapboxgl.Marker()
+            .setLngLat(coords)
+            .addTo(map);
+        map.setCenter(coords);
+    });
+});
